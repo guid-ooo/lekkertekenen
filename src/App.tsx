eb703b5416f081e26c50f900a910332d54dfa047
Guido <gevertzen@kaliber.net>
2025-12-08 09:00:49 +0100

    added padding to wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -407,7 +407,7 @@ const DrawingApp = () => {
         </div>
       </div>
       
-          <div className="w-full h-full flex items-center justify-center max-h-[80dvh]"  style={{ aspectRatio: "800 / 480"}}>
+          <div className="w-full h-full flex items-center justify-center max-h-[80dvh] p-4"  style={{ aspectRatio: "800 / 480"}}>
               <canvas
                 ref={canvasRef}
                 width={800}
